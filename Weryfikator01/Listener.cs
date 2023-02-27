b3fe15e420384c5444b57b908931a5bc22c98471
Piotr Osłowski <piotr.oslowski@scapaflow.pl>
2023-02-27 19:28:33 +0100

    dodany weryfikator na PozycjaDokHandlowego (sprawdza długość pola SWW)

--- Weryfikator01/Listener.cs
+++ Weryfikator01/Listener.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Soneta.Business;
 using Soneta.Business.App;
+using Soneta.Handel;
 using Soneta.Kadry;
+using static Soneta.Handel.HandelModule;
 
 [assembly: Service(typeof(ILoginListener), typeof(Weryfikator01.Listener), ServiceScope.Login)]
 namespace Weryfikator01
@@ -26,23 +28,36 @@ namespace Weryfikator01
                 Row.Session.Verifiers.Add(new ImieNazwiskoWeryfikator(Row as Pracownik));
             }
             );
-            Soneta.Kadry.KadryModule.PracownikSchema.AddOnEditing((Soneta.Kadry.KadryModule.PracownikRow Row) => 
-            { 
-                Row.Session.Verifiers.Add(new ImieNazwiskoWeryfikator(Row as Pracownik)); 
+            Soneta.Kadry.KadryModule.PracownikSchema.AddOnEditing((Soneta.Kadry.KadryModule.PracownikRow Row) =>
+            {
+                Row.Session.Verifiers.Add(new ImieNazwiskoWeryfikator(Row as Pracownik));
             }
             );
 
+
+            Soneta.Handel.HandelModule.PozycjaDokHandlowegoSchema.AddOnAdded((PozycjaDokHandlowegoRow Row) =>
+            {
+                Row.Session.Verifiers.Add(new PozDokHanglowegoWeryfikator(Row as PozycjaDokHandlowego));
+
+            }
+            );
+            Soneta.Handel.HandelModule.PozycjaDokHandlowegoSchema.AddOnEditing((PozycjaDokHandlowegoRow Row) =>
+            {
+                Row.Session.Verifiers.Add(new PozDokHanglowegoWeryfikator(Row as PozycjaDokHandlowego));
+
+            }
+            );
 
         }
 
         public void BeforeLogin(Session session)
         {
-            
+
         }
 
         public void BeforeUnlogin(Login login)
         {
-            
+
         }
     }
 }
---
+++ Weryfikator01/PozDokHanglowegoWeryfikator.cs
@@ -0,0 +1,40 @@
+﻿using Mono.CSharp;
+using Soneta.Business;
+using Soneta.Handel;
+using Soneta.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weryfikator01
+{
+    public class PozDokHanglowegoWeryfikator : RowVerifier<PozycjaDokHandlowego>
+    {
+        public PozDokHanglowegoWeryfikator(PozycjaDokHandlowego row) : base(row)
+        {
+        }
+
+        public override string Description => "sww nie może być krótsze niz 5 znaków";
+
+        protected override bool IsValid()
+        {
+
+            PozycjaDokHandlowego poz = this.Row as PozycjaDokHandlowego;
+
+            if (poz == null)
+            {
+                return true;
+            }
+            if (poz.SWW.Length<5)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override VerifierType Type => VerifierType.Warning;
+    }
+}
